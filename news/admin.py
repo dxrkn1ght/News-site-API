be1a91e2e3637385b1590c89c3c83f269ef48052
dxrkn1ght <komronbekzubaydullayev@gmail.com>
2025-03-09 17:35:25 +0500

    Project has finished

--- news/admin.py
+++ news/admin.py
@@ -9,10 +9,3 @@ class AuthorAdmin(admin.ModelAdmin):
     list_filter = ('created_at',)
     ordering = ('created_at',)
 
-# category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='news')
-# tags = models.ManyToManyField(Tag, related_name='news')
-# image = models.ImageField(null=True)
-# views_count = models.PositiveIntegerField(default=0)
-# is_published = models.BooleanField(default=False)
-# created_at = models.DateTimeField(auto_now_add=True)
-# updated_at = models.DateTimeField(auto_now=True)
--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import New
 
-# Register your models here.
+
+@admin.register(New)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name','slug')
+    search_fields = ('name', )
+    list_filter = ('name',)
+    ordering = ('name',)
+
